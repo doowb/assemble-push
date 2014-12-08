9227c15171407d381b7c788dee7290f1e2b19784
Brian Woodward <brian.woodward@gmail.com>
2014-12-08 16:30:43 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-push <https://github.com/doowb/assemble-push>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
